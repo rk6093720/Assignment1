0713e19c8536b8f1e1d355c73dc70e03013f76c8
rk6093720 <97551067+rk6093720@users.noreply.github.com>
2022-11-06 23:38:49 +0530

    done
    done

---
+++ Notes app/backend/routes/notes.routes.js
@@ -0,0 +1,61 @@
+const {Router} = require("express")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+
+const {NoteModel} = require("../models/Note.model")
+
+const notesController = Router();
+
+
+notesController.get("/", async (req, res) => {
+    const {tag} = req.query
+    const notes = await NoteModel.find({userId : req.body.userId, tag})
+    res.send(notes)
+})
+
+
+notesController.post("/create", async (req, res) => {
+    const {Heading, Note, Tag, userId} = req.body;
+    const note = new NoteModel({
+        Heading,
+        Note,
+        Tag,
+        userId
+    })
+    try{
+        await note.save()
+        res.send("note created")
+    }
+    catch(err){
+        res.send("something went wrong")
+    }
+})
+
+
+notesController.delete("/delete/:noteId", async (req, res) => {
+    const {noteId} = req.params
+    const deletedNote = await NoteModel.findOneAndDelete({_id : noteId, userId : req.body.userId})
+    if(deletedNote){
+        res.status(200).send("Deleted")
+    }
+    else{
+        res.send("couldn't delete")
+    }
+})
+
+notesController.patch("/edit/:noteId", async (req, res) => {
+    const {noteId} = req.params
+    const deletedNote = await NoteModel.findOneAndUpdate({_id : noteId, userId : req.body.userId},req.body)
+    if(deletedNote){
+        res.send("Deleted")
+    }
+    else{
+        res.send("couldn't delete")
+    }
+})
+
+
+module.exports = {
+    notesController
+}
---
+++ Notes app/backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const cors = require("cors")
+const {userController} = require("./routes/user.routes")
+const {notesController} = require("./routes/notes.routes")
+const {connection} = require("./config/db")
+const {authentication} = require("./middlewares/authentication")
+
+const app = express();
+const PORT = 8080
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Home page")
+})
+
+app.use(cors())
+
+app.use("/user", userController)
+app.use(authentication)
+app.use("/notes", notesController)
+
+app.listen(PORT, async () => {
+    try{
+        await connection;
+        console.log("Connected to db")
+    }
+    catch(err){
+        console.log("Error connnecting to DB")
+        console.log(err)
+    }
+    console.log(`listening on PORT ${PORT}`)
+})
